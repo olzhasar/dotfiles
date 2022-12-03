68758f591fe91c87faea4584e140862071420018
olzhasar <o.arystanov@gmail.com>
2022-12-04 04:40:17 +0600

    Add LocalHistory

--- config/nvim/lua/core/commands.lua
+++ config/nvim/lua/core/commands.lua
@@ -3,3 +3,14 @@ local create_command = vim.api.nvim_create_user_command
 create_command("Vimrc", ":e $MYVIMRC", {})
 create_command("SVimrc", ":luafile $MYVIMRC", {})
 create_command("Bd", "bp|bd #", {})
+
+vim.cmd([[
+  command! -bang LocalHistory call fzf#run({
+    \ 'source': fzf#vim#_recent_files(),
+    \ 'sink': 'e',
+    \ 'options': [
+    \   '--query', '!^~ !^/ !^.git/ ',
+    \   '--prompt', 'FileHist> ',
+    \   '--multi',
+    \ ]}, <bang>0)
+]])
--- config/nvim/lua/core/keymaps.lua
+++ config/nvim/lua/core/keymaps.lua
@@ -38,7 +38,7 @@ map("n", "<Leader>ag", ":Ag<CR>")
 map("n", "<Leader>rg", ":Rg<CR>")
 map("n", "<C-s>", ":Rg <C-R><C-W><CR>")
 map("n", "<Leader>l", ":Lines<CR>")
-map("n", "<Leader>h", ":History<CR>")
+map("n", "<Leader>h", ":LocalHistory<CR>")
 
 --Trouble
 map("n", "<Leader>tr", ":TroubleToggle<CR>")
