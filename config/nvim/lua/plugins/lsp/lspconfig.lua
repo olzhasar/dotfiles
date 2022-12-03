bfa7a86822632c3261200a2d59733b992f6e598d
olzhasar <o.arystanov@gmail.com>
2022-12-04 04:01:45 +0600

    Update lspconfig

--- config/nvim/lua/plugins/lsp/lspconfig.lua
+++ config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -46,6 +46,11 @@ end
 lspconfig["pyright"].setup({
   capabilities = capabilities,
   on_attach = on_attach,
+  settings = {
+    ["pyright"] = {
+      ["typeCheckingMode"] = "off",
+    }
+  }
 })
 
 -- configure html server
@@ -93,36 +98,3 @@ lspconfig["sumneko_lua"].setup({
     },
   },
 })
-
--- Filter Pyright diagnostics
-local function filter(arr, func)
-  -- Filter in place
-  -- https://stackoverflow.com/questions/49709998/how-to-filter-a-lua-array-inplace
-  local new_index = 1
-  local size_orig = #arr
-  for old_index, v in ipairs(arr) do
-    if func(v, old_index) then
-      arr[new_index] = v
-      new_index = new_index + 1
-    end
-  end
-  for i = new_index, size_orig do
-    arr[i] = nil
-  end
-end
-
-local function filter_diagnostics(diagnostic)
-  -- Only filter out Pyright stuff for now
-  if diagnostic.source == "Pyright" then
-    return false
-  end
-
-  return true
-end
-
-local function custom_on_publish_diagnostics(a, params, client_id, c, config)
-  filter(params.diagnostics, filter_diagnostics)
-  vim.lsp.diagnostic.on_publish_diagnostics(a, params, client_id, c, config)
-end
-
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(custom_on_publish_diagnostics, {})
