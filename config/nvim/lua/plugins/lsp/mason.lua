cb51995aebe8d5a8701818cb8b60ccc5b0765028
olzhasar <o.arystanov@gmail.com>
2022-12-03 19:48:34 +0600

    Update mason config

--- config/nvim/lua/plugins/lsp/mason.lua
+++ config/nvim/lua/plugins/lsp/mason.lua
@@ -37,12 +37,10 @@ mason_lspconfig.setup({
 mason_null_ls.setup({
   -- list of formatters & linters for mason to install
   ensure_installed = {
-    "black",
-    "isort",
     "prettier", -- ts/js formatter
     "stylua", -- lua formatter
     "eslint_d", -- ts/js linter
   },
   -- auto-install configured formatters & linters (with null-ls)
-  automatic_installation = true,
+  automatic_installation = false,
 })
