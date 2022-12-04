61204eb5ce77831e84faa6462466c1df1f7dc0be
olzhasar <o.arystanov@gmail.com>
2022-12-04 16:11:17 +0600

    Update bufferline config

--- config/nvim/lua/plugins/bufferline.lua
+++ config/nvim/lua/plugins/bufferline.lua
@@ -3,4 +3,14 @@ if not setup then
   return
 end
 
-bufferline.setup()
+bufferline.setup({
+  options = {
+    show_buffer_close_icons = false,
+    sort_by='id',
+    custom_filter = function(buf_number, buf_numbers)
+      if vim.bo[buf_number].filetype ~= "netrw" then
+      	return true
+      end
+    end,
+  }
+})
