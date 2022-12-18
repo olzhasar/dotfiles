2da3755e8146593a2a0f79fdaae8c657851dbf6f
olzhasar <o.arystanov@gmail.com>
2022-12-18 19:28:10 +0600

    Update vimwiki highlights

--- config/nvim/lua/plugins/configs/vimwiki.lua
+++ config/nvim/lua/plugins/configs/vimwiki.lua
@@ -5,3 +5,9 @@ vim.g.vimwiki_list = { {
 } }
 
 vim.g.vimwiki_global_ext = 0
+vim.api.nvim_set_hl(0, "VimwikiHeader1", {fg="#005f87"})
+vim.api.nvim_set_hl(0, "VimwikiHeader2", {fg="#d70000"})
+vim.api.nvim_set_hl(0, "VimwikiHeader3", {fg="#8700af"})
+vim.api.nvim_set_hl(0, "VimwikiHeader4", {fg="#005f87"})
+vim.api.nvim_set_hl(0, "VimwikiHeader5", {fg="#878787"})
+vim.api.nvim_set_hl(0, "VimwikiHeader6", {fg="#bcbcbc"})
