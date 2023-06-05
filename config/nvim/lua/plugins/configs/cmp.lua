113a18f5189a68f71c6f2bd61791c788c141a169
olzhasar <o.arystanov@gmail.com>
2023-06-06 02:19:04 +0600

    Update cmp config

--- config/nvim/lua/plugins/configs/cmp.lua
+++ config/nvim/lua/plugins/configs/cmp.lua
@@ -12,11 +12,11 @@ local lspkind_status, lspkind = pcall(require, "lspkind")
 if not lspkind_status then
   return
 end
-
-local has_words_before = function()
-  local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
+--
+-- local has_words_before = function()
+--   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+--   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+-- end
 
 vim.opt.completeopt = "menu,menuone,noselect"
 
@@ -46,8 +46,6 @@ cmp.setup({
         cmp.select_next_item()
       elseif luasnip.expand_or_jumpable() then
         luasnip.expand_or_jump()
-      elseif has_words_before() then
-        cmp.complete()
       else
         fallback()
       end
