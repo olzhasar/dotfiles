b9217a76e78694519af89f62cbd9708fe7ceccf8
olzhasar <o.arystanov@gmail.com>
2023-06-13 17:34:20 +0600

    Update treesitter config

--- config/nvim/lua/plugins/configs/treesitter.lua
+++ config/nvim/lua/plugins/configs/treesitter.lua
@@ -5,7 +5,7 @@ end
 
 treesitter.setup({
   -- enable syntax highlighting
-  highlight = { enable = false, disable = {"python"} },
+  highlight = { enable = false },
   -- enable indentation
   indent = { enable = false },
   -- enable autotagging (w/ nvim-ts-autotag plugin)
@@ -63,9 +63,9 @@ treesitter.setup({
       -- and should return the mode ('v', 'V', or '<c-v>') or a table
       -- mapping query_strings to modes.
       selection_modes = {
-        ['@parameter.outer'] = 'v', -- charwise
-        ['@function.outer'] = 'V', -- linewise
-        ['@class.outer'] = '<c-v>', -- blockwise
+        ["@parameter.outer"] = "v", -- charwise
+        ["@function.outer"] = "V", -- linewise
+        ["@class.outer"] = "<c-v>", -- blockwise
       },
       -- If you set this to `true` (default is `false`) then any textobject is
       -- extended to include preceding or succeeding whitespace. Succeeding
@@ -76,7 +76,7 @@ treesitter.setup({
       -- * query_string: eg '@function.inner'
       -- * selection_mode: eg 'v'
       -- and should return true of false
-      include_surrounding_whitespace = true,
+      include_surrounding_whitespace = false,
     },
     move = {
       enable = true,
@@ -85,36 +85,25 @@ treesitter.setup({
         ["]m"] = "@function.outer",
         ["]]"] = { query = "@class.outer", desc = "Next class start" },
         --
-        -- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queries.
-        ["]o"] = "@loop.*",
-        -- ["]o"] = { query = { "@loop.inner", "@loop.outer" } }
-        --
         -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
         -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
         ["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
         ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
       },
-      goto_next_end = {
-        ["]M"] = "@function.outer",
-        ["]["] = "@class.outer",
-      },
       goto_previous_start = {
         ["[m"] = "@function.outer",
         ["[["] = "@class.outer",
+        ["[s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
+        ["[z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+      },
+      goto_next_end = {
+        ["]M"] = "@function.outer",
+        ["]["] = "@class.outer",
       },
       goto_previous_end = {
         ["[M"] = "@function.outer",
         ["[]"] = "@class.outer",
       },
-      -- Below will go to either the start or the end, whichever is closer.
-      -- Use if you want more granular movements
-      -- Make it even more gradual by adding multiple queries and regex.
-      goto_next = {
-        ["]d"] = "@conditional.outer",
-      },
-      goto_previous = {
-        ["[d"] = "@conditional.outer",
-      }
     },
   },
 })
