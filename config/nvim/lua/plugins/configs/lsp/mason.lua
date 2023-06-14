9b86d337229c7cf76f111c75c6a949d7609d95fb
olzhasar <o.arystanov@gmail.com>
2023-06-15 05:14:09 +0600

    Update mason config

--- config/nvim/lua/plugins/configs/lsp/mason.lua
+++ config/nvim/lua/plugins/configs/lsp/mason.lua
@@ -32,6 +32,12 @@ mason_lspconfig.setup({
     "dockerls",
     "gopls",
     "jsonls",
+    "sqlls",
+    "yamlls",
+    "ansiblels",
+    "terraformls",
+    "texlab",
+    "bashls",
   },
   -- auto-install configured servers (with lspconfig)
   automatic_installation = true, -- not the same as ensure_installed
