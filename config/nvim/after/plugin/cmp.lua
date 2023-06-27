b3cefba9bfe31d05619d21676dd255b5ed5f640c
olzhasar <o.arystanov@gmail.com>
2023-06-28 00:51:22 +0600

    Add sorting section to cmp config

--- config/nvim/after/plugin/cmp.lua
+++ config/nvim/after/plugin/cmp.lua
@@ -1,4 +1,4 @@
-local cmp =require("cmp")
+local cmp = require("cmp")
 local luasnip = require("luasnip")
 local lspkind = require("lspkind")
 local cmp_dict = require("cmp_dictionary")
@@ -72,6 +72,33 @@ cmp.setup({
         luasnip = "[SNIP]",
       },
     }),
+  },
+
+  sorting = {
+    comparators = {
+      cmp.config.compare.offset,
+      cmp.config.compare.exact,
+      cmp.config.compare.score,
+      cmp.config.compare.recently_used,
+
+      -- Move items starting with underscore to the end
+      function(entry1, entry2)
+        local _, entry1_under = entry1.completion_item.label:find("^_+")
+        local _, entry2_under = entry2.completion_item.label:find("^_+")
+        entry1_under = entry1_under or 0
+        entry2_under = entry2_under or 0
+        if entry1_under > entry2_under then
+          return false
+        elseif entry1_under < entry2_under then
+          return true
+        end
+      end,
+
+      cmp.config.compare.kind,
+      cmp.config.compare.sort_text,
+      cmp.config.compare.length,
+      cmp.config.compare.order,
+    },
   },
 })
 
