79006b0d511c158f50bd63470835ec51a08e4726
olzhasar <o.arystanov@gmail.com>
2023-06-29 20:01:28 +0600

    Auto switch to light colorscheme when editing blog

--- config/nvim/lua/core/colorscheme.lua
+++ config/nvim/lua/core/colorscheme.lua
@@ -16,4 +16,15 @@ end
 
 vim.api.nvim_create_user_command("BackgroundSwitch", switch_background, {})
 
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = { "content/*.md" },
+  callback = function(_)
+    if vim.o.background == "dark" then
+      set_colorscheme(light_colorscheme, "light")
+      require("zen-mode").toggle()
+    end
+  end,
+  group = vim.api.nvim_create_augroup("colorscheme", { clear = true }),
+})
+
 set_colorscheme(dark_colorscheme, "dark")
