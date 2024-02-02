0378bfdf818bec89504ab489d93c78d6b1680fc8
olzhasar <o.arystanov@gmail.com>
2024-02-02 18:40:27 +0800

    Update netrw_list_hide

--- config/nvim/lua/core/options.lua
+++ config/nvim/lua/core/options.lua
@@ -55,7 +55,7 @@ vim.g.netrw_banner = 0
 vim.g.netrw_liststyle = 3
 vim.g.netrw_bufsettings = "noma nomod nu nobl nowrap ro"
 vim.g.netrw_list_hide =
-  ".*.swp$,.*.pyc,.pytest_cache,.mypy_cache,__pycache__,.tags$,node_modules,.*.sqlite,celerybeat-schedule$"
+  ".*.swp$,.*.pyc,.pytest_cache,.mypy_cache,.ruff_cache,__pycache__,.tags$,node_modules,.*.sqlite,celerybeat-schedule$"
 
 --python
 vim.g.python3_host_prog = "/Users/olzhas/.pyenv/shims/python3"
