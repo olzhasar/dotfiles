1197bdf48e36d8d12c68b185a23d108e4024f807
olzhasar <o.arystanov@gmail.com>
2024-02-15 13:40:01 +0600

    Update nvim config

--- config/nvim/lua/core/options.lua
+++ config/nvim/lua/core/options.lua
@@ -53,6 +53,8 @@ o.statusline = o.statusline .. "%l:%c "
 -- netrw
 vim.g.netrw_banner = 0
 vim.g.netrw_liststyle = 3
+vim.g.netrw_fastbrowse = 0
+vim.g.netrw_altfile = 1
 vim.g.netrw_bufsettings = "noma nomod nu nobl nowrap ro"
 vim.g.netrw_list_hide =
   ".*.swp$,.*.pyc,.pytest_cache,.mypy_cache,.ruff_cache,__pycache__,.tags$,node_modules,.*.sqlite,celerybeat-schedule$"
