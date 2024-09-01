7e7ee552bb8e4195392f48da3c3939d27b6609a0
olzhasar <o.arystanov@gmail.com>
2024-09-01 23:36:49 +0500

    Do not switch background in .md files

--- config/nvim/lua/core/autocmd.lua
+++ config/nvim/lua/core/autocmd.lua
@@ -42,3 +42,11 @@ api.nvim_create_autocmd("BufWritePre", {
   command = "silent! call mkdir(fnamemodify(expand('<afile>'), ':p:h'), 'p')",
   group = au_group,
 })
+
+api.nvim_create_autocmd("BufEnter", {
+  pattern = { "content/*.md" },
+  callback = function(_)
+    require("zen-mode").toggle()
+  end,
+  group = au_group,
+})
