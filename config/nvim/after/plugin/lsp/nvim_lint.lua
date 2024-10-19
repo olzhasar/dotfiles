c803a022d1119d719bac50724877ddd4372d56cf
olzhasar <o.arystanov@gmail.com>
2024-10-20 04:18:42 +0500

    Fix nvim lint config

--- config/nvim/after/plugin/lsp/nvim_lint.lua
+++ config/nvim/after/plugin/lsp/nvim_lint.lua
@@ -1,10 +1,12 @@
-require("lint").linters_by_ft = {
+local lint = require("lint")
+
+lint.linters_by_ft = {
   python = { "mypy" },
   markdown = { "vale" },
 }
 
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
   callback = function()
-    require("lint").try_lint()
+    lint.try_lint(nil, { ignore_errors = true })
   end,
 })
