bc8aa913d9f8c12034b676f7f2e17075529112fe
olzhasar <o.arystanov@gmail.com>
2024-11-24 08:00:26 +0500

    Update keymaps

--- config/nvim/lua/plugins/fugitive.lua
+++ config/nvim/lua/plugins/fugitive.lua
@@ -1,3 +1,15 @@
 return {
   "tpope/vim-fugitive",
+  config = function()
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "fugitive" },
+      callback = function()
+        vim.schedule(function()
+          vim.keymap.set("n", "<leader>gp", ":Git push<CR>", { buffer = true })
+          vim.keymap.set("n", "<leader>gF", ":Git push --force<CR>", { buffer = true })
+          vim.keymap.set("n", "<leader>gP", ":Git pull --rebase<CR>", { buffer = true })
+        end)
+      end,
+    })
+  end,
 }
