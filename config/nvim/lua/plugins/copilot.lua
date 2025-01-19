1a1cabc104038953b091611102ffcfd97805d15b
olzhasar <o.arystanov@gmail.com>
2025-01-19 19:50:51 +0800

    Update nvim packages

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -12,7 +12,6 @@ return {
   },
   {
     "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "canary",
     dependencies = {
       { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
       { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
--- config/nvim/lua/plugins/filetype.lua
+++ config/nvim/lua/plugins/filetype.lua
@@ -5,10 +5,11 @@ return {
   { "lervag/vimtex", ft = "tex" },
   {
     "iamcco/markdown-preview.nvim",
-    build = "cd app && npm install",
-    ft = { "markdown" },
-    config = function()
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
       vim.g.mkdp_filetypes = { "markdown" }
     end,
+    ft = { "markdown" },
   },
 }
