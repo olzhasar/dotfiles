9c2b736e856925b0d06e5377fa72fd7523ae9b5e
olzhasar <o.arystanov@gmail.com>
2026-06-24 01:59:38 +0700

    Fix ctrl-i in kitty and nvim

--- config/nvim/lua/core/keymaps.lua
+++ config/nvim/lua/core/keymaps.lua
@@ -25,6 +25,7 @@ map("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- Buffers
 map("n", "<Leader>bd", ":Bd<CR>") -- close current buffer
+map("n", "<C-i>", "<C-i>")
 map("n", "<Tab>", "<C-^>")
 map("n", "<C-n>", ":bnext<CR>")
 
